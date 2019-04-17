4dd1ec46fbf994c79a848abf94f52b7e579f1156
Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>
2019-04-18 00:14:14 +0430

    C# sample codes I used for learning C#.
    Signed-off-by: Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>

---
+++ Module 1/module1/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //below is the attributes for a student
+            string st_fname = "john", st_lname = "stuart";
+            DateTime st_birthdate = new DateTime(1989, 05, 2);
+            string st_addline1 = "somwhere street";
+            string st_addline2 = "first alley";
+            string st_city = "London";
+            string st_country = "england";
+            string st_state = "some state";
+            int st_zipcode = 10007;
+            //below is the attributes for a professor
+            string pf_fname = "edward", pf_lname = "denilson";
+            DateTime pf_birthdate = new DateTime(1949, 11, 12);
+            string pf_addline1 = "somwhere street";
+            string pf_addline2 = "third alley";
+            string pf_city = "London";
+            string pf_country = "england";
+            string pf_state = "some state";
+            int pf_zipcode = 12007;
+            string pf_field = "machine learning";
+            string pf_degree = "Associate professor";
+            //below we provide some information about a university program
+            string pg_name = "Electrical engineering";
+            string pg_degrees = "bachelor of science";
+            string dp_head = "david fry";
+            //below a course information is provided
+            string cs_name = "Electrical circuits 1";
+            short cs_credit = 3;
+            string cs_type = "full time";
+            Console.WriteLine("student information are as follows:");
+            Console.WriteLine("Name and surname: {0} {1}.", st_fname, st_lname);
+            Console.WriteLine("Student's address: {0}, {1}, {2}, {3}.", st_addline1, st_addline2, st_city, st_country);
+            Console.WriteLine("student's state and zip code, respectively: {0}, {1}.", st_state, st_zipcode);
+            Console.WriteLine();
+            Console.WriteLine("a professor information are as follows:");
+            Console.WriteLine("professor's name and surname: {0}, {1}.", pf_fname, pf_lname);
+            Console.WriteLine("professors's address: {0}, {1}, {2}, {3}.", pf_addline1, pf_addline2, pf_city, pf_country);
+            Console.WriteLine("professor's state and zip code, respectively: {0}, {1}.", pf_state, pf_zipcode);
+            Console.WriteLine("professor's field of interest and degree, respectively: {0}, {1}.", pf_field, pf_degree);
+            Console.WriteLine();
+            Console.WriteLine("information regarding a university program:");
+            Console.WriteLine("program name: {0}, study level: {1}.", pg_name, pg_degrees);
+            Console.WriteLine("the department head is: {0}.", dp_head);
+            Console.WriteLine();
+            Console.WriteLine("a typical course description is as follows:");
+            Console.WriteLine("course name: {0}, total credit of the course is {1} and the type of study is {2}.", cs_name, cs_credit, cs_type);
+
+
+
+        }
+    }
+}
+
+//using System;
+
+//namespace Assignment
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string firstName, lastName, birthday, addressLine1, addressLine2,
+//            city, stateProvince, country, programName, degreeOffered, departmentHead;
+//            int zipOrPostal;
+//            firstName = "CHAN";
+//            lastName = "CHI SHING";
+//            birthday = "11/12/1995";
+//            addressLine1 = "ROOM 18 WING ON BUILDING";
+//            addressLine2 = "32 CANTON ROAD KOWLOON";
+//            city = "HONG KONG";
+//            stateProvince = "GUANGDONG";
+//            country = "CHINA";
+//            zipOrPostal = 852;
+//            programName = "Bachelor of Computer Science";
+//            degreeOffered = "Honor Degree";
+//            departmentHead = "JOHN CHEUNG";
+//            Console.WriteLine("========================================");
+//            Console.WriteLine("First Name: {0}", firstName);
+//            Console.WriteLine("Last Name: {0}", lastName);
+//            Console.WriteLine("Birthday: {0}", birthday);
+//            Console.WriteLine("Address Line 1: {0}", addressLine1);
+//            Console.WriteLine("Address Line 2: {0}", addressLine2);
+//            Console.WriteLine("City: {0}", city);
+//            Console.WriteLine("State/Province: {0}", stateProvince);
+//            Console.WriteLine("Country: {0}", country);
+//            Console.WriteLine("Zip/Postal: {0}", zipOrPostal);
+//            Console.WriteLine("Program name: {0}", programName);
+//            Console.WriteLine("Degrees offered: {0}", degreeOffered);
+//            Console.WriteLine("Department Head: {0}", departmentHead);
+//            Console.WriteLine("========================================");
+//        }
+//    }
+//}
+
+/**
+* April 17th, 2015
+*/
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpCourse
+//{
+
+//    public class Assignment01
+//    {
+
+//        public static void Main(string[] args)
+//        {
+
+//            //Student's Personal Information
+//            string name = "Jonathan";
+//            string surname = "Brown";
+//            int age = 31;
+
+//            //Student's Educational Background
+//            string country = "the United Kingdom";
+//            string degree = "BA in Biology";
+//            string university = "University of Manchester";
+
+//            //Student's Economic Situation
+//            double gdp = 1342.37;
+//            bool parent = false;
+
+//            Console.WriteLine("The student's full name is " + name + " " + surname + ".");
+//            Console.WriteLine(name + " " + surname + " is " + age + " years old, " +
+//            "lives in " + country + ", and got his " + degree +
+//            " from the " + university + ".");
+//            Console.WriteLine("The student's GDP is " + gdp + " and any assertion that " +
+//            name + " " + surname + " is a parent is " + parent + ".");
+//        }
+//    }
+//}
---
+++ Module 6/Module 6/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating 3 students:
+            Student st1 = new Student("john");
+            Student st2 = new Student("barbara");
+            Student st3 = new Student();            
+            // Instantiating a course with the specified name:
+            Course cs1 = new Course("Programming with C#");
+            //Adding students to the object course:
+            cs1.St[0] = st1;
+            cs1.St[1] = st2;
+            cs1.St[2] = st3;
+            //Instantiating 3 teacher objects:
+            Teacher TA1 = new Teacher("Smith");
+            Teacher TA2 = new Teacher("katrine");
+            Teacher TA3 = new Teacher("Katrine", "Smith");
+            //Adding teachers to the object course:
+            cs1.Ta[0] = TA1;
+            cs1.Ta[1] = TA2;
+            cs1.Ta[2] = TA3;
+            //Instantiating a Bachelor degree
+            Degree dg = new Degree("Bachelor of Science");
+            //Adding the instantiated course to the degree
+            dg.Cs = cs1;
+            //Instantiating a university program called IT:
+            UProgram UP = new UProgram("Information Technology");
+            //Adding the instantiated degree to the university program:
+            UP.Deg = dg;
+            //Outputting the requested data using the Console.WriteLine method:
+            Console.WriteLine("The program name is {0} and it contains the {1} degree.", UP.Name, dg.Name);
+            Console.WriteLine("The {0} degree contains the course {1}.", dg.Name, cs1.Name);
+            Console.WriteLine("The number of students enrolled in the {0} course: {1}.", cs1.Name, Student.GetNumOfStudent());
+            Console.ReadKey();
+        }
+    }
+    // Defining the base class person:
+    public class Person
+    {
+        //using automatic properties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Birthday { get; set; }
+        public string Addline1 { get; set; }
+        public string Addline2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Zip { get; set; }
+        // using defined properties:
+        private string state;
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+            }
+        }
+        public Person(String name = "Blank",
+            String surname = "Blank",
+            String birthday = "Blank",
+            String addline1 = "Blank",
+            String addline2 = "Blank",
+            int zip = 0,
+            String city = "Blank",
+            String state = "Blank",
+            String country = "Blank")
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Birthday = birthday;
+            this.Addline1 = addline1;
+            this.Addline2 = addline2;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.Country = country;
+        }
+    }
+    // Defining Teacher class based on person class:
+    public class Teacher : Person
+    {
+        // Properties applly to teachers only:
+        public string Rank { get; set; }
+        public void GradeTest()
+        {
+            // Functionality is not provided in the instructions.
+            throw new NotImplementedException();
+        }
+        public Teacher(String name = "Blank",
+            String surname = "Blank",
+            String birthday = "Blank",
+            String addline1 = "Blank",
+            String addline2 = "Blank",
+            int zip = 0,
+            String city = "Blank",
+            String state = "Blank",
+            String country = "Blank",
+            string rank = "Assosiate professor")
+            : base(name,surname)
+        {
+            this.Rank = rank;
+        }
+    }
+    public class Student : Person 
+    {
+        //Student specefic properties:
+        public string TypeOfStudy { get; set; }
+        // The following field holds the number of student objects created so far.
+        private static int NumOfStudents = 0;
+        public Student(String name = "Blank",
+            String surname = "Blank",
+            String birthday = "Blank",
+            String addline1 = "Blank",
+            String addline2 = "Blank",
+            int zip = 0,
+            String city = "Blank",
+            String state = "Blank",
+            String country = "Blank",
+            string type = "Full Time")
+            : base(name, surname)
+        {
+            this.TypeOfStudy = type;
+            NumOfStudents += 1;
+        }
+        public static int GetNumOfStudent()
+        {
+            return NumOfStudents;
+        }
+        public void TakeTest()
+        {
+            //Functionality for this code is not provided in the instructions
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        private Student[] st = new Student[3];
+        public Student[] St
+        {
+            get
+            {
+                return st;
+            }
+            set
+            {
+                st = value;
+            }
+        }
+        private Teacher[] ta = new Teacher[3];      //The array to hold 3 teachers 
+        public Teacher[] Ta                         // The property implementation
+        {
+            get
+            {
+                return ta;
+            }
+            set
+            {
+                ta = value;
+            }
+        }
+        public Course(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    public class Degree
+    {
+        public string Name { get; set; }
+        public Course Cs { get; set; } //This property (Cs) will hold the course 
+        public Degree(String name)
+        {
+            this.Name = name;
+        }
+    }
+
+    public class UProgram
+    {
+        public string Name { get; set; }
+        public Degree Deg { get; set; } //This property (Deg) will hold the degree class 
+        public UProgram(string name)
+        {
+            this.Name = name;
+        }
+
+    }
+}
